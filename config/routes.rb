88bff82e7f73e4a344e63ec6e26dd202f055c0e8
Roshan <icemission@gmail.com>
2015-09-25 19:21:08 +0800

    Updates application controller to prevent CSRF exception from being raised

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   namespace :api, defaults: { format: :json }, path: '/' do
     scope module: :v1 do
+      devise_for :users
       resources :users, :only => [:show]
     end
   end
