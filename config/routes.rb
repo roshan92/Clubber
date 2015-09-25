848627cc553db6b6dc99f1dc4df29d7a0b93f553
Roshan <icemission@gmail.com>
2015-09-25 19:26:44 +0800

    Adds destroy action to the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, path: '/' do
     scope module: :v1 do
       devise_for :users
-      resources :users, only: [:show, :create, :update]
+      resources :users, only: [:show, :create, :update, :destroy]
     end
   end
 end
