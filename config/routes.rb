be3a8e0766d1aafc47d820b8537d3a73143e53e6
Roshan <icemission@gmail.com>
2015-09-25 19:24:06 +0800

    Adds the user create endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, path: '/' do
     scope module: :v1 do
       devise_for :users
-      resources :users, :only => [:show]
+      resources :users, only: [:show, :create]
     end
   end
 end
