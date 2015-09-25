664439678e8883b1d2640cb4a9e6ab24fe2dd749
Roshan <icemission@gmail.com>
2015-09-25 18:53:07 +0800

    Adds devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   namespace :api, defaults: { format: :json }, path: '/' do
-    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+    scope module: :v1 do
 
     end
   end
