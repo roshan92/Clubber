d2f44e92a9eb6e0dada93f14dbb79a2c91a35c2e
Roshan <icemission@gmail.com>
2015-09-25 19:00:02 +0800

    Adds show action the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api, defaults: { format: :json }, path: '/' do
     scope module: :v1 do
-
+      resources :users, :only => [:show]
     end
   end
 end
