7b189c29680b21d27fd3236d3a1ffcac54dadd4a
Roshan <icemission@gmail.com>
2015-09-28 18:03:00 +0800

    Embeds the user into the product json output

--- app/serializers/offer_serializer.rb
+++ app/serializers/offer_serializer.rb
@@ -1,3 +1,4 @@
 class OfferSerializer < ActiveModel::Serializer
-  attributes :id, :price, :status, :payment, :user_id, :deal_open_hour, :deal_closed_hour, :quantity, :created_at, :updated_at
+  attributes :id, :price, :status, :payment, :deal_open_hour, :deal_closed_hour, :quantity, :created_at, :updated_at
+  has_one :user
 end
