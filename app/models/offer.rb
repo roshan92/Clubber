9432769af628a1c4bba6632499a353799fd535e4
Roshan <icemission@gmail.com>
2015-09-28 18:10:29 +0800

    Adds search scopes on the offers model

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -5,4 +5,20 @@ class Offer < ActiveRecord::Base
 
 	validates :item_id, :deal_open_hour, :deal_closed_hour, presence: true
   validates :price, :quantity, numericality: { greater_than_or_equal_to: 0 }, presence: true
+
+  # scope :filter_by_title, lambda { |keyword|
+  #   where("lower(title) LIKE ?", "%#{keyword.downcase}")
+  # }
+  scope :above_or_equal_to_price, lambda { |price|
+    where("price >= ?", price)
+  }
+
+  scope :below_or_equal_to_price, lambda { |price|
+    where("price <= ?", price)
+  }
+
+  scope :recent, -> {
+    order(:updated_at)
+  }
+
 end
