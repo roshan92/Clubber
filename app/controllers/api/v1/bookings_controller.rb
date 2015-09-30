02461d3e9f0d41d1824ea0813d8cf89536faf33d
Roshan <icemission@gmail.com>
2015-09-30 17:59:59 +0800

    Added api doc for Booking

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -1,26 +1,46 @@
 class Api::V1::BookingsController < ApplicationController
-  before_action :authenticate_with_token!, only: [:create, :update]
+  before_action :authenticate_with_token!, only: [:create, :update, :destroy]
   # before_action :set_booking, only: [:show, :edit, :update, :destroy]
 
   respond_to :json
 
+  api :GET, '/bookings', "Show all bookings"
   def index
-    respond_with current_user.bookings
+    bookings = params[:booking_ids].present? ? Booking.find(params[:booking_ids]) : Booking.all
+    respond_with bookings
   end
 
+  api :GET, '/bookings/:id', "Show a single booking"
   def show
-    respond_with current_user.bookings.find(params[:id])
+    respond_with Booking.find(params[:id])
+
+  rescue ActiveRecord::RecordNotFound
+    render json: { errors: 'No such booking exist' }, status: 422
   end
 
+  api :POST, '/bookings', "Create a booking"
+  param :booking, Hash, desc: "Booking Information" do
+    param :offer_id, :number, desc: "Offer ID", required: true
+    param :quantity, :number, desc: 'Quantity', required: true
+    param :amount, :number, desc: 'Amount', required: true
+  end
   def create
     booking = current_user.bookings.build(booking_params)
     if booking.save
-      render json: booking, status: 201, location: [:api, current_user, booking]
+      render json: booking, status: 201, location: [:api, booking]
     else
       render json: { errors: booking.errors }, status: 422
     end
   end
 
+  api :PUT, '/bookings/:id', "Update a booking"
+  param :booking, Hash, desc: "Booking Information" do
+    param :offer_id, :number, desc: "Offer ID", required: true
+    param :quantity, :number, desc: 'Quantity'
+    param :amount, :number, desc: 'Amount'
+    param :paid, :bool, decs: 'Payment status, default: false'
+    param :paid_on, DateTime, desc: 'Payment date'
+  end
   def update
     booking = current_user.bookings.find(params[:id])
     if booking.update(booking_params)
@@ -28,12 +48,19 @@ class Api::V1::BookingsController < ApplicationController
     else
       render json: { errors: booking.errors }, status: 422
     end
+
+  rescue ActiveRecord::RecordNotFound
+    render json: { errors: 'No such booking exist' }, status: 422
   end
 
+  api :DELETE, '/bookings/:id', "Delete a booking"
   def destroy
-    booking = current_user.bookings.find(params[:id])
+    booking = Booking.find(params[:id])
     booking.destroy
     head 204
+
+  rescue ActiveRecord::RecordNotFound
+    render json: { errors: 'No such booking exist' }, status: 422
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,11 @@ Clubber::Application.routes.draw do
 
   namespace :api, defaults: { format: :json }, path: '/api/v1' do
     scope module: :v1 do
-      resources :users, only: [:show, :create, :update, :destroy, :index] do
-        resources :bookings, only: [:create, :update ,:destroy]
-      end
+      resources :users, only: [:show, :create, :update, :destroy, :index]
       resources :sessions, only: [:create, :destroy]
       resources :offers, only: [:create, :update ,:destroy, :show, :index]
       resources :items, only: [:create, :update ,:destroy, :show, :index]
-      resources :bookings, only: [:show, :index]
+      resources :bookings, only: [:create, :update ,:destroy, :show, :index]
       resources :events, only: [:create, :update ,:destroy, :show, :index]
       resources :invites, only: [:create, :update ,:destroy, :show, :index]
     end
