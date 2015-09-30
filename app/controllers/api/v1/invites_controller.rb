f1ba043a8e569b9a7c9f63931d4b8d228edc7c11
Roshan <icemission@gmail.com>
2015-09-30 17:36:15 +0800

    Added api doc for invites

--- app/controllers/api/v1/invites_controller.rb
+++ app/controllers/api/v1/invites_controller.rb
@@ -1,19 +1,28 @@
 class Api::V1::InvitesController < ApplicationController
-	before_action :authenticate_with_token!, only: [:create, :update]
+	before_action :authenticate_with_token!, only: [:create, :update, :destroy]
 
 	respond_to :json
 
+	api :GET, '/invites', "Shows all invites"
   def index
-    invites = params[:invite_ids].present? ? invite.find(params[:invite_ids]) : invite.all
+    invites = params[:invite_ids].present? ? Invite.find(params[:invite_ids]) : Invite.all
     respond_with invites
-
     # respond_with event.search(params)
   end
 
+	api :GET, '/invites/:id', "Show a single invite"
   def show
-    respond_with invite.find(params[:id])
+    respond_with Invite.find(params[:id])
+
+	rescue ActiveRecord::RecordNotFound
+		render json: { errors: 'No such invite exist' }, status: 422
   end
 
+	api :POST, '/invites', "Create an invite"
+	param :invite, Hash, desc: "Invite Information" do
+		param :attended_event_id, :number, desc: "Attended Event ID", required: true
+		param :guest_id, :number, desc: 'Guest ID', required: true
+	end
   def create
     invite = current_user.invites.build(invite_params)
     if invite.save
@@ -23,6 +32,11 @@ class Api::V1::InvitesController < ApplicationController
     end
   end
 
+	api :PUT, '/invites/:id', "Update an invite"
+	param :invite, Hash, desc: "Invite Information" do
+		param :attended_event_id, :number, desc: "Attended Event ID", required: true
+		param :guest_id, :number, desc: 'Guest ID', required: true
+	end
   def update
     invite = current_user.invites.find(params[:id])
     if invite.update(invite_params)
@@ -30,12 +44,19 @@ class Api::V1::InvitesController < ApplicationController
     else
       render json: { errors: invite.errors }, status: 422
     end
+
+	rescue ActiveRecord::RecordNotFound
+		render json: { errors: 'No such invite exist' }, status: 422
   end
 
+	api :DELETE, '/invites/:id', "Delete an invite"
   def destroy
-    invite = current_user.invites.find(params[:id])
+		invite = Invite.find(params[:id])
     invite.destroy
     head 204
+
+  rescue ActiveRecord::RecordNotFound
+    render json: { errors: 'No such invite exist' }, status: 422
   end
 
   private
